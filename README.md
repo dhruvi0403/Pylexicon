e42766d92689f0012c876561b733ac6e9a2fe870
= <dhruvishah0403@gmail.com>
2019-11-18 23:15:53 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # Pylexicon
-Pylexicon is a command-line dictionary tool. Users can get antonyms, synonyms, definitions, examples just by passing a word in the command line.
+Pylexicon is a command-line dictionary tool. The simple use case of this tool is users can get antonyms, synonyms, definitions, examples just by passing a word in the command line and another major feature is one can play the game, you have to guess a word from the given definition, synonym or an antonym.
+
+## Pre-requisites
+- Python3.6+ version is required
+- First of all, create .env file and declare two variables:
+	API_KEY = your_api_key
+	API_HOST = https://fourtytwowords.herokuapp.com
+
+
+## The command-line tool has the following functions - 
+
+1. Word Definitions
+
+            python3 dictionarytool.py defn <word>
+
+Will display definitions of a given word.
+
+2. Word Synonyms
+
+            python3 dictionarytool.py syn <word>
+
+Will display synonyms of a given word. 
+
+3. Word Antonyms
+
+            python3 dictionarytool.py ant <word>
+
+Will display antonyms of a given word.
+
+4. Word Examples
+
+            python3 dictionarytool.py ex <word>
+
+Will display examples of usage of a given word in a sentence. 
+
+5. Word Full Dict
+
+            python3 dictionarytool.py <word>
+
+Will display Word Definitions, Word Synonyms, Word Antonyms & Word Examples for a given word.
+
+6. Word of the Day Full Dict
+
+            python3 dictionarytool.py
+
+Will display Word Definitions, Word Synonyms, Word Antonyms & Word Examples for a random word.
+
+7. Word Game
+
+            python3 dictionarytool.py play
+
+The command will display a definition, a synonym or an antonym and ask the user to guess the word. 
---
+++ dictionarytool.py
@@ -0,0 +1,211 @@
+"""
+This Python file is created for building Command Line Dictionary Tool.
+"""
+import sys
+import os
+import random
+from random import shuffle
+import re
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+
+# Environment Variables
+API_KEY = os.getenv('API_KEY')
+API_HOST = os.getenv('API_HOST')
+
+# Global Variables
+definition_url = API_HOST+'/word/{}/definitions?api_key='+API_KEY
+antonym_url = API_HOST+'/word/{}/relatedWords?api_key='+API_KEY
+example_url = API_HOST+'/word/{}/examples?api_key='+API_KEY
+randomwords_url = API_HOST+'/words/randomWord?api_key='+API_KEY
+special = []
+syn = []
+ant = []
+definitions = []
+all_list = []
+hint = []
+examples = []
+count = 0
+s = requests.Session()
+
+class DictionaryTool():
+    """Class is created to perform different dictionary operations."""
+    def __init__(self):
+        pass
+
+    def shuffle_word(self, word):
+        """To display the word randomly jumbled (cat => atc, tac, tca)"""
+        word = list(word)
+        shuffle(word)
+        return ''.join(word)
+
+    def play(self, count):
+        """The command should display a definition, a synonym or an antonym and ask the user to guess the word."""
+        del all_list[:]
+        del hint[:]
+        if count == 0:
+            synonym, antonym, word = obj.random_words(1)
+            all_list.extend(synonym)
+            definition = requests.get(definition_url.format(word))
+            definition_json = definition.json()
+            for i in range(0, len(definition_json)):
+                all_list.append(definition_json[i]['text'])
+            if antonym != 'No antonyms found':
+                all_list.extend(antonym)
+            s.question = random.choice(all_list)
+            print(s.question)
+            s.result = ''
+            result = input('Enter the correct word: ')
+        result = ''
+        if result == word or result in synonym or s.result == word or s.result in synonym:
+            print('Successfully solved.')
+            return
+        else:
+            print('Incorrect word.Choose from the following: \n 1. Try again 2. Hint 3. Quit')
+            try:
+                incorrect_result = int(input())
+            except ValueError:
+                incorrect_result = input('Which one 1,2 or 3?')
+            if incorrect_result == 1:
+                s.result = input("Try again: ")
+                count += 1
+                obj.play(count)
+            elif incorrect_result == 2:
+                hint.extend([obj.shuffle_word(word) for word in word.split(',')])
+                hint.extend(all_list)
+                hint.remove(s.question)
+                if s.result != '':
+                    hint.remove(s.result)
+                random_hint = random.choice(hint)
+                s.result = input(random_hint)
+                count += 1
+                obj.play(count)
+            elif incorrect_result == 3:
+                if antonym == 'No antonyms found':
+                    print('Correct Word: {} \nSynonyms: {} \nDefinitions:'.format(word, synonym))
+                    # print(*definitions, sep='\n')
+                else:
+                    print('Correct Word: {} \nSynonyms: {} \nAntonyms: {}\nDefinitions:'.format(word, synonym, antonym))
+                    # print(*definitions, sep='\n')
+                return
+
+    def definition(self, word):
+        """Display definitions of a given word."""
+        request_url = (definition_url.format(word))
+        response = requests.get(request_url)
+        data = response.json()
+        del definitions[:]
+        for i, val in enumerate(data):
+            definitions.append('{}) {}'.format(i+1, val['text']))
+        return definitions
+
+    def synonym_antonym(self, syn_ant, word):
+        """Display synonyms/antonyms of a given word."""
+        request_url = (antonym_url.format(word))
+        response = requests.get(request_url)
+        data = response.json()
+        if response.status_code == 200:
+            for i, val in enumerate(data):
+                if val['relationshipType'] == 'synonym' and syn_ant == 'syn':
+                    syn.extend(val['words'])
+                    return syn
+                if val['relationshipType'] == 'antonym' and syn_ant == 'ant':
+                    ant.extend(val['words'])
+                    return ant
+                elif val['relationshipType'] != 'antonym' and syn_ant == 'ant':
+                    return 'No antonyms found'
+        elif response.status_code == 400:
+            return data['error']
+
+    def example(self, word):
+        """Display examples of usage of a given word in a sentence."""
+        request_url = (example_url.format(word))
+        response = requests.get(request_url)
+        data = response.json()
+        if response.status_code == 200:
+            del special[:]
+            for i, val in enumerate(data['examples']):
+                clean_text = (re.sub('[^a-zA-Z0-9]+', ' ', val['text']))
+                special.append('{}) {}'.format(i+1, clean_text))
+                examples.append(clean_text)
+            return special
+        elif response.status_code == 400:
+            return data['error']
+
+    def display(self, word):
+        """Display Word Definitions, Word Synonyms, Word Antonyms & Word Examples for a given word."""
+        ex = obj.example(word)
+        if ex != 'word not found':
+            print('Examples:')
+            # print(*ex, sep='\n')
+            definition = obj.definition(word)
+            print('\nDefinition:')
+            # print(*definition, sep='\n')
+            synonym = obj.synonym_antonym('syn', word)
+            print('\nSynonyms:')
+            # print(*synonym, sep=',')
+            antonym = obj.synonym_antonym('ant', word)
+            if antonym != 'No antonyms found':
+                print('\nAntonyms:')
+                # print(*antonym, sep=', ')
+        else:
+            print(ex)
+
+    def random_words(self, play):
+        """Display Word Definitions, Word Synonyms, Word Antonyms & Word Examples for a random word."""
+        response = requests.get(randomwords_url)
+        if response.status_code == 200:
+            data = response.json()
+            ex = obj.example(data['word'])
+            if ex != 'word not found':
+                definition = obj.definition(data['word'])
+                synonym = obj.synonym_antonym('syn', data['word'])
+                antonym = obj.synonym_antonym('ant', data['word'])
+                print('Examples:')
+                # print(*ex, sep='\n')
+                print('Definition:')
+                # print(*definition, sep='\n')
+                print('Synonyms:')
+                # print(*synonym, sep=',')
+                if antonym != 'No antonyms found':
+                    print('Antonyms:')
+                    # print(*antonym, sep=', ')
+                if play == 1:
+                    word = data['word']
+                    return synonym, antonym, word
+
+obj = DictionaryTool()
+# ./dict
+if len(sys.argv) == 1:
+    obj.random_words(0)
+
+else:
+    # ./dict play
+    if sys.argv[1] == 'play':
+        obj.play(count)
+
+    # ./dict defn <word>
+    if sys.argv[1] == 'defn':
+        definition = obj.definition(sys.argv[2])
+        # if definition != 'word not found':
+        #     print(*definition, sep='\n')
+        # else:
+        #     print(definition)
+
+    # ./dict syn <word> or ./dic ant <word>
+    if sys.argv[1] == 'syn' or sys.argv[1] == 'ant':
+        syn_ant = obj.synonym_antonym(sys.argv[1], sys.argv[2])
+        print(syn_ant)
+
+    # ./dict ex <word>
+    if sys.argv[1] == 'ex':
+        ex = obj.example(sys.argv[2])
+        # if ex != 'word not found':
+        #     print(*ex, sep='\n')
+        # else:
+        #     print(ex)
+
+    # ./dict <word>
+    else:
+        obj.display(sys.argv[1])
